97cab258f6327dbbe44f6c491d5797b32b21c54d
Nicola Beghin <nicolabeghin@gmail.com>
2021-05-06 22:04:28 +0200

    remove deploy.sh

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-export JAVA_HOME=/Library/Java/JavaVirtualMachines/liberica-jdk-11.0.2/Contents/Home/
-
-mvn package
-cp target/spid-provider.jar ~/keycloak-sia/deployments/spid-provider.jar
-docker cp target/spid-provider.jar $(docker ps -q --filter 'name=keycloak'):/opt/jboss/keycloak/standalone/deployments/
