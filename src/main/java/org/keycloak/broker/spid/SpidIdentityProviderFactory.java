759b361a58767100464c737da70cc82eac6dd9e1
Bart Lievens <Bart.Lievens@unifiedpost.com>
2022-02-16 23:14:17 +0100

    Extend SpidIdentityProviderConfig from SAMLIdentityProviderConfig

--- src/main/java/org/keycloak/broker/spid/SpidIdentityProviderFactory.java
+++ src/main/java/org/keycloak/broker/spid/SpidIdentityProviderFactory.java
@@ -26,7 +26,6 @@ import javax.xml.namespace.QName;
 
 import org.keycloak.Config.Scope;
 import org.keycloak.broker.provider.AbstractIdentityProviderFactory;
-import org.keycloak.common.util.Time;
 import org.keycloak.dom.saml.v2.assertion.AttributeType;
 import org.keycloak.dom.saml.v2.metadata.EndpointType;
 import org.keycloak.dom.saml.v2.metadata.EntitiesDescriptorType;
