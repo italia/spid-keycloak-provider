192d14eeea0378d291f02891cc3cd318a2627e81
polifr <polifr@gmail.com>
2026-05-15 08:41:20 +0200

    Script di compilazione del provider per compose

---
+++ kc-spid-compose/build-provider-jar.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+docker run -it --rm \
+  -v $PWD/..:/opt/maven \
+  -v $HOME/.m2:/opt/maven-repo/.m2 \
+  -w /opt/maven maven:3-eclipse-temurin-17 mvn \
+  -Duser.home=/opt/maven-repo \
+  -Dmaven.test.skip=true \
+  -Ddependency-check.skip=true \
+  clean install
+
+cp $PWD/../target/spid-provider.jar $PWD/provider/spid-provider.jar
