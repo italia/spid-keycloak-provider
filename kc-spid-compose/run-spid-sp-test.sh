59a181bb3855465a96e5925b57d0b8d878680272
polifr <polifr@gmail.com>
2026-05-15 13:58:04 +0200

    Fix nome immagine di spid-sp-test

--- kc-spid-compose/run-spid-sp-test.sh
+++ kc-spid-compose/run-spid-sp-test.sh
@@ -5,4 +5,4 @@ docker pull ghcr.io/italia/spid-sp-test:latest
 docker run -ti --rm \
   -v "$(pwd)/tests/metadata:/spid/mymetadata:ro" \
   -v "$(pwd)/tests/dumps:/spid/dumps:rw" \
-  italia/spid-sp-test:latest --metadata-url http://localhost:8180/realms/master/broker/spid-saml/endpoint/spid-keycloak-other.xml
+  ghcr.io/italia/spid-sp-test:latest --metadata-url http://localhost:8180/realms/master/broker/spid-saml/endpoint/spid-keycloak-other.xml
