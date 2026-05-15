ec786055f960bbca63ee377f0796d03ba9b907ff
polifr <polifr@gmail.com>
2026-05-15 10:18:55 +0200

    Bozza di script per esecuzione test da spid-sp-test

---
+++ kc-spid-compose/run-spid-sp-test.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+docker pull ghcr.io/italia/spid-sp-test:latest
+
+docker run -ti --rm \
+  -v "$(pwd)/tests/metadata:/spid/mymetadata:ro" \
+  -v "$(pwd)/tests/dumps:/spid/dumps:rw" \
+  italia/spid-sp-test:latest --metadata-url http://localhost:8180/realms/master/broker/spid-saml/endpoint/spid-keycloak-other.xml
