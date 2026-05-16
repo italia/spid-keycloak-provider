f760143cfce0f2f9842297806f61245ec799ab7c
polifr <polifr@gmail.com>
2026-05-17 00:31:40 +0200

    Rimozione creazione keystore

--- kc-spid-compose/create-self-signed-certificates.sh
+++ kc-spid-compose/create-self-signed-certificates.sh
@@ -4,9 +4,3 @@
 openssl req -newkey rsa:2048 -nodes \
   -keyout $PWD/certificates/keycloak-server.key.pem -x509 -days 3650 \
   -out $PWD/certificates/keycloak-server.crt.pem
-
-# Keystore generation
-openssl pkcs12 -export \
-  -in $PWD/certificates/keycloak-server.crt.pem \
-  -inkey $PWD/certificates/keycloak-server.key.pem \
-  -out $PWD/certificates/keycloak-server.p12
