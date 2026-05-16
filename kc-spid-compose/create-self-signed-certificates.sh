828b09f6e36d865de74996b85cca91216b0f501d
polifr <polifr@gmail.com>
2026-05-17 00:15:38 +0200

    Creazione del keystore dai certificati

--- kc-spid-compose/create-self-signed-certificates.sh
+++ kc-spid-compose/create-self-signed-certificates.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+# Key and crt files generation
 openssl req -newkey rsa:2048 -nodes \
   -keyout $PWD/certificates/keycloak-server.key.pem -x509 -days 3650 \
   -out $PWD/certificates/keycloak-server.crt.pem
+
+# Keystore generation
+openssl pkcs12 -export \
+  -in $PWD/certificates/keycloak-server.crt.pem \
+  -inkey $PWD/certificates/keycloak-server.key.pem \
+  -out $PWD/certificates/keycloak-server.p12
