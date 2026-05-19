f54294df7d6741845dd1f305b01391ae0cb1000c
polifr <polifr@gmail.com>
2026-05-19 23:52:12 +0200

    Modifica puntamento per generazione metadata da parte di spid-sp-test

--- kc-spid-compose/create-spid-sp-test-metadata.sh
+++ kc-spid-compose/create-spid-sp-test-metadata.sh
@@ -2,7 +2,7 @@
 
 docker pull ghcr.io/italia/spid-sp-test:latest
 
-rm -rf $PWD/tests/spid-sp-test.xml
+rm -rf $PWD/tests/metadata/spid-sp-test.xml
 
 # Key and crt files generation
-docker run --rm -it ghcr.io/italia/spid-sp-test --idp-metadata > $PWD/tests/spid-sp-test.xml
+docker run --rm -it ghcr.io/italia/spid-sp-test --idp-metadata > $PWD/tests/metadata/spid-sp-test.xml
