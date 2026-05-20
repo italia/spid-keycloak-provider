2a30871ccb39db0ccb65fca7ef046b66d989f7f7
Francesco Poli <polifr@gmail.com>
2026-05-20 11:44:19 +0200

    Fix su creazione directory non esistente

--- kc-spid-compose/create-spid-sp-test-metadata.sh
+++ kc-spid-compose/create-spid-sp-test-metadata.sh
@@ -3,6 +3,7 @@
 docker pull ghcr.io/italia/spid-sp-test:latest
 
 rm -rf $PWD/tests/metadata/spid-sp-test.xml
+mkdir -p $PWD/tests/metadata
 
 # Key and crt files generation
 docker run --rm -it ghcr.io/italia/spid-sp-test --idp-metadata > $PWD/tests/metadata/spid-sp-test.xml
