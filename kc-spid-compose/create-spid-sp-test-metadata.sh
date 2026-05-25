be25a2dd974d72d03cd96496235ce2815aa01b4b
Francesco Poli <polifr@gmail.com>
2026-05-25 21:11:02 +0200

    Rimozione righe di errore dall'xml metadati

--- kc-spid-compose/create-spid-sp-test-metadata.sh
+++ kc-spid-compose/create-spid-sp-test-metadata.sh
@@ -4,5 +4,6 @@ rm -rf $PWD/tests/spid-sp-test.xml
 
 # spid-sp-test.xml medadata file generation
 docker run --net=host --rm -t italia/spid-sp-test:0.9.0 --idp-metadata \
+  | sed '/^[[:space:]]*</!d' \
   | sed 's/WantAuthnRequestsSigned="false"/WantAuthnRequestsSigned="true" WantAssertionsSigned="false"/' \
   > $PWD/tests/spid-sp-test.xml
