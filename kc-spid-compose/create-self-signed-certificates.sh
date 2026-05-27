239c3cc0863ebe0ee26ad5241ac4a05b06b988da
Francesco Poli <polifr@gmail.com>
2026-05-28 00:35:33 +0200

    Modifica attributi certificati self-signed

--- kc-spid-compose/create-self-signed-certificates.sh
+++ kc-spid-compose/create-self-signed-certificates.sh
@@ -6,7 +6,7 @@ openssl req -x509 -newkey rsa:4096 -sha256 -days 3650 -nodes \
   -keyout $PWD/certificates/keycloak-server.key \
   -out $PWD/certificates/keycloak-server.crt \
   -subj "/C=IT/ST=MI/L=Milan/O=AgID/OU=Keycloak/CN=keycloak" \
-  -addext "subjectAltName = DNS:spid-keycloak, DNS:localhost:8443"
+  -addext "subjectAltName = DNS:spid-keycloak, DNS:spid-keycloak:8443, DNS:localhost:8443"
 
 # Key and crt files generation for nginx (ref. nicolabeghin Makefile)
 rm -rf $PWD/certificates/nginx.key $PWD/certificates/nginx.crt
@@ -14,4 +14,4 @@ openssl req -x509 -newkey rsa:4096 -sha256 -days 3650 -nodes \
   -keyout $PWD/certificates/nginx.key \
   -out $PWD/certificates/nginx.crt \
   -subj "/C=IT/ST=MI/L=Milan/O=AgID/OU=Servizio Accreditamento/CN=nginx" \
-  -addext "subjectAltName = DNS:spid-nginx, DNS:localhost:8443"
+  -addext "subjectAltName = DNS:spid-nginx, DNS:spid-nginx:443, DNS:localhost:443"
