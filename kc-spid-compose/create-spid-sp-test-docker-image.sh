12d06cffa7c960448759a53c26400dcf778650d7
Francesco Poli <polifr@gmail.com>
2026-05-30 21:52:05 +0200

    Commentati step di debug

--- kc-spid-compose/create-spid-sp-test-docker-image.sh
+++ kc-spid-compose/create-spid-sp-test-docker-image.sh
@@ -10,8 +10,9 @@ cd $PWD/tmp-docker-spid-sp-test
 git clone -b fixes-2025 https://github.com/nicolabeghin/spid-sp-test.git
 
 SPID_SP_TEST_CERTIFICATE=$(cat ./spid-sp-test/src/spid_sp_test/responses/certificates/test_public.cert | grep -v CERTIFICATE | tr -d '\n')
-echo SPID_SP_TEST_CERTIFICATE: ${SPID_SP_TEST_CERTIFICATE}
+# echo SPID_SP_TEST_CERTIFICATE: ${SPID_SP_TEST_CERTIFICATE}
 sed -i "s#<ds:X509Certificate />#<ds:X509Certificate>${SPID_SP_TEST_CERTIFICATE}</ds:X509Certificate>#" ./spid-sp-test/src/spid_sp_test/responses/settings.py
+# cat ./spid-sp-test/src/spid_sp_test/responses/settings.py | grep "<ds:X509Certificate>"
 
 docker build --tag italia/spid-sp-test:0.9.0-KC ./spid-sp-test
 
