771115c935f1afa6c06dd11165f3e33b1927fd5e
Francesco Poli <polifr@gmail.com>
2026-06-03 21:30:03 +0200

    Parametrizzazione versione Java da usare

--- kc-spid-compose/build-provider-jar.sh
+++ kc-spid-compose/build-provider-jar.sh
@@ -1,16 +1,19 @@
 #!/bin/sh
 
+JAVA_RELEASE=17
+
 rm -rf $PWD/provider/spid-provider.jar
 
-docker pull maven:3-eclipse-temurin-17
+docker pull maven:3-eclipse-temurin-$JAVA_RELEASE
 
 docker run -it --rm \
   -v $PWD/..:/opt/maven \
   -v $HOME/.m2:/opt/maven-repo/.m2 \
-  -w /opt/maven maven:3-eclipse-temurin-17 mvn \
+  -w /opt/maven maven:3-eclipse-temurin-$JAVA_RELEASE mvn \
   -Duser.home=/opt/maven-repo \
   -Dmaven.test.skip=true \
   -Ddependency-check.skip=true \
+  -Dmaven.compiler.release=$JAVA_RELEASE \
   clean install
 
 cp $PWD/../target/spid-provider.jar $PWD/provider/spid-provider.jar
