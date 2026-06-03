5fb16dd313067ce323a6864a8c2da10eacf3169e
Francesco Poli <polifr@gmail.com>
2026-06-03 22:35:05 +0200

    Generazione certificati idp aggiornati

--- kc-spid-compose/create-spid-sp-test-metadata.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-rm -rf $PWD/tests/spid-sp-test.xml
-
-# spid-sp-test.xml medadata file generation
-docker run --net=host --rm -t italia/spid-sp-test:0.9.0-KC --idp-metadata \
-  | sed '/^[[:space:]]*</!d' \
-  | sed 's/WantAuthnRequestsSigned="false"/WantAuthnRequestsSigned="true" WantAssertionsSigned="false"/' \
-  > $PWD/tests/spid-sp-test.xml
