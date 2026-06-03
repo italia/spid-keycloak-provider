582f67c06beb095febfbfd78dc0dfdc848ac167e
Francesco Poli <polifr@gmail.com>
2026-06-03 23:38:52 +0200

    Uso del Makefile del progetto per la compilazione del jar del provider

--- kc-spid-compose/build-provider-jar.sh
+++ kc-spid-compose/build-provider-jar.sh
@@ -4,16 +4,6 @@ JAVA_RELEASE=17
 
 rm -rf $PWD/provider/spid-provider.jar
 
-docker pull maven:3-eclipse-temurin-$JAVA_RELEASE
-
-docker run -it --rm \
-  -v $PWD/..:/opt/maven \
-  -v $HOME/.m2:/opt/maven-repo/.m2 \
-  -w /opt/maven maven:3-eclipse-temurin-$JAVA_RELEASE mvn \
-  -Duser.home=/opt/maven-repo \
-  -Dmaven.test.skip=true \
-  -Ddependency-check.skip=true \
-  -Dmaven.compiler.release=$JAVA_RELEASE \
-  clean install
+make -C $PWD/.. build
 
 cp $PWD/../target/spid-provider.jar $PWD/provider/spid-provider.jar
