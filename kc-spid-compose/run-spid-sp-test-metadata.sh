0f6b78438b44f3a552c6c959e95dbc32abc34f9e
Francesco Poli <polifr@gmail.com>
2026-06-03 22:35:17 +0200

    Utilizzo certificati idp aggiornati nei test

--- kc-spid-compose/run-spid-sp-test-metadata.sh
+++ kc-spid-compose/run-spid-sp-test-metadata.sh
@@ -4,6 +4,8 @@ NOW=$(date +"%Y%m%d_%H%M%S")
 mkdir -p $(pwd)/tests/report/${NOW}_metadata
 
 docker run -ti --net=host --rm \
+  -e IDP_CERT_PATH=/spid/ipd-certs \
+  -v "$(pwd)/certificates/spid-sp-test-idp:/spid/ipd-certs" \
   -v "$(pwd)/tests/report/${NOW}_metadata:/spid/html_report:rw" \
   italia/spid-sp-test:0.9.0-KC \
   --metadata-url "https://localhost:8443/auth/realms/spid/spid-sp-metadata" \
--- kc-spid-compose/run-spid-sp-test-responses.sh
+++ kc-spid-compose/run-spid-sp-test-responses.sh
@@ -7,6 +7,8 @@ mkdir -p $(pwd)/tests/report/${NOW}_responses
 mkdir -p $(pwd)/tests/dumps/${NOW}_responses
 
 docker run -ti --net=host --rm \
+  -e IDP_CERT_PATH=/spid/ipd-certs \
+  -v "$(pwd)/certificates/spid-sp-test-idp:/spid/ipd-certs" \
   -v "$(pwd)/tests/report/${NOW}_responses:/spid/html_report:rw" \
   -v "$(pwd)/tests/dumps/${NOW}_responses:/spid/dumps:rw" \
   italia/spid-sp-test:0.9.0-KC \
