db4e7217113a51447b5e0e3ef3acdcbd4b7cb5be
Francesco Poli <polifr@gmail.com>
2026-06-03 14:42:43 +0200

    Differenziazione delle directory di output dei test per data

--- kc-spid-compose/run-spid-sp-test-metadata.sh
+++ kc-spid-compose/run-spid-sp-test-metadata.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
-docker run --net=host --rm \
-  -v "$(pwd)/tests/report:/spid/html_report:rw" \
-  -v "$(pwd)/tests/dumps:/spid/dumps:rw" \
-  -t italia/spid-sp-test:0.9.0-KC \
-  --metadata-url https://localhost:8443/auth/realms/spid/spid-sp-metadata
+NOW=$(date +"%Y%m%d_%H%M%S")
+
+docker run -ti --net=host --rm \
+  -v "$(pwd)/tests/report/${NOW}:/spid/html_report:rw" \
+  -v "$(pwd)/tests/dumps/${NOW}:/spid/dumps:rw" \
+  italia/spid-sp-test:0.9.0-KC \
+  --metadata-url "https://localhost:8443/auth/realms/spid/spid-sp-metadata" \
+  --extra -rf html -o html_report/ --response-html-dumps dumps/
--- kc-spid-compose/run-spid-sp-test-responses.sh
+++ kc-spid-compose/run-spid-sp-test-responses.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
-docker run --net=host --rm \
-  -v "$(pwd)/tests/report:/spid/html_report:rw" \
-  -v "$(pwd)/tests/responses:/spid/responses:rw" \
-  -v "$(pwd)/tests/dumps:/spid/dumps:rw" \
-  -t italia/spid-sp-test:0.9.0-KC --test-response \
-  --metadata-url https://localhost:8443/auth/realms/spid/spid-sp-metadata \
-  --authn-url "https://localhost:8443/auth/realms/spid/protocol/openid-connect/auth?client_id=account&scope=openid&response_type=code&redirect_uri=https://localhost:8443/auth/realms/spid/account&state=12345&kc_idp_hint=spid-spid-sp-test"
+NOW=$(date +"%Y%m%d_%H%M%S")
+
+docker run -ti --net=host --rm \
+  -v "$(pwd)/tests/report/${NOW}:/spid/html_report:rw" \
+  -v "$(pwd)/tests/dumps/${NOW}:/spid/dumps:rw" \
+  italia/spid-sp-test:0.9.0-KC \
+  --metadata-url "https://localhost:8443/auth/realms/spid/spid-sp-metadata" \
+  --authn-url "https://localhost:8443/auth/realms/spid/protocol/openid-connect/auth?client_id=account&scope=openid&response_type=code&redirect_uri=https://localhost:8443/auth/realms/spid/account&state=12345&kc_idp_hint=spid-spid-sp-test" \
+  --test-response \
+  --extra -rf html -o html_report/ --response-html-dumps dumps/
