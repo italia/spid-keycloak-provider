a5b51aefc3988aa2a5a609900c91f88b5be0c2db
Francesco Poli <polifr@gmail.com>
2026-06-06 01:24:31 +0200

    Spostamento test ed avviamento compose nel Makefile

--- kc-spid-compose/start-mariadb-compose.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-SPID_KC_DB_TYPE=mariadb docker compose \
-  -f docker-compose-networks.yaml \
-  -f docker-compose-mariadb.yaml \
-  -f docker-compose-keycloak.yaml \
-  -f docker-compose-nginx.yaml \
-  up
--- kc-spid-compose/start-mysql-compose.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-SPID_KC_DB_TYPE=mysql docker compose \
-  -f docker-compose-networks.yaml \
-  -f docker-compose-mysql.yaml \
-  -f docker-compose-keycloak.yaml \
-  -f docker-compose-nginx.yaml \
-  up
